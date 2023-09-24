90b0647eb698cba219f043550c02f4504c29f173
petrovkarmm <petrovleonid1999@mail.ru>
2023-09-24 19:51:34 +0500

    create new url api/v1/product and endpoint for him ProductListAPIView

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from .views.get_products_list import ProductListAPIView
+
 app_name = 'products'
 
 urlpatterns = [
-    path('test-products/', admin.site.urls),
+    path('api/v1/product/', ProductListAPIView.as_view(), name='products-api'),
 ]
