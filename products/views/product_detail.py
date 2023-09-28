36b9b3a0d180e4e7b5195bc6b0c5273e46be4eaf
petrovkarmm <petrovleonid1999@mail.ru>
2023-09-28 21:15:18 +0500

    add into product_detail view product PATCH for admins.

--- products/views/product_detail.py
+++ products/views/product_detail.py
@@ -1,7 +1,10 @@
+from rest_framework import status
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from products.models.product import Product
+from products.serializers.product_list_serializer_admin import ProductListSerializerAdmin
 from products.serializers.product_serializer import ProductSerializer
 
 
@@ -11,3 +14,16 @@ class ProductAPIView(APIView):
         serializer = ProductSerializer(product, context={'request': request})
 
         return Response(serializer.data)
+
+    def patch(self, request, product_id):
+        self.permission_classes = [IsAdminUser]
+        product = get_object_or_404(Product, id=product_id)
+        serializer = ProductListSerializerAdmin(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            product = serializer.save()
+            serializer_to_response = ProductSerializer(product)
+            response_data = {
+                'Product after update': serializer_to_response.data
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
